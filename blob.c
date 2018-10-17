307d8f46b52f9394b279ac4f94795918cf7455db
Jeff King <peff@peff.net>
2018-10-17 00:41:05 -0400

    parse_blob_buffer(): mark unused buffer/size parameters
    These could be dropped, but even if we don't use them, it's
    important that the caller _have_ them, since marking
    "parsed" here is supposed to mean we definitely have the
    object.
    
    Signed-off-by: Jeff King <peff@peff.net>

--- blob.c
+++ blob.c
@@ -13,7 +13,9 @@ struct blob *lookup_blob(struct repository *r, const struct object_id *oid)
 	return object_as_type(obj, OBJ_BLOB, 0);
 }
 
-int parse_blob_buffer(struct blob *item, void *buffer, unsigned long size)
+int parse_blob_buffer(struct blob *item,
+		      void *UNUSED(buffer),
+		      unsigned long UNUSED(size))
 {
 	item->object.parsed = 1;
 	return 0;
